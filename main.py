725cbb2bd260afad59f45192b3fb16dc99e83fe6
AmateurHotShot <59028016+AmateurHotShot@users.noreply.github.com>
2021-05-24 19:29:33 -0600

    increased post limit and added subreddit

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ import edit_videos
 import post_videos
 import delete_videos
 
-subs = ['Unexpected', ]
+subs = ['Unexpected', 'Whatcouldgowrong', ]
+post_limit = 10
 
-find_videos.GetPosts(subs, post_limit=6).download_posts()
+find_videos.GetPosts(subs, post_limit=post_limit).download_posts()
 edit_videos.EditClip.shortify()
 post_videos.YouTubeUpload.upload()
 delete_videos.DeleteClips.delete()
